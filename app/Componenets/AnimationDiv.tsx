a90f508dba37be2564dada536534b3bbc0dd5a09
bachidze <bachidze90@gmail.com>
2024-09-27 18:39:55 +0400

    Feat(Add):Finihs

--- app/Componenets/AnimationDiv.tsx
+++ app/Componenets/AnimationDiv.tsx
@@ -12,29 +12,16 @@ export default function AnimationDiv() {
   const [isHovered, setIsHovered] = useState(false); 
 
   return (
-    <main className="relative overflow-hidden border-2 pt-6 pl-[60px] pb-[65px] bg-black">
-      <motion.div 
-        className="absolute bottom-0"
-        initial={{ scale: 1 }} 
-        whileHover={{ scale: 2 }} 
-        transition={{ duration: 2 }}
-        onHoverStart={() => setIsHovered(true)}
-        onHoverEnd={() => setIsHovered(false)}
-      >
-        <motion.div 
-          className="w-0 h-0 cursor-pointer border-l-[100vw] border-l-transparent border-r-[100vw] border-r-transparent border-b-[20vh]  hover:border-b-[220vh] transition-all duration-1000"
-        />
-      </motion.div>
-      
-      <section className={`flex justify-between relative z-20 ${isHovered ? 'text-black' : 'text-white'}`}>
+    <main 
+      className={`relative overflow-hidden border-2 pt-6 pl-[60px] pb-[65px] bg-black transition-colors duration-300 ${isHovered ? 'text-black' : 'text-white'}`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <section className="flex justify-between relative z-20">
         <div>
           <h1 className="text-[36px] font-medium leading-normal uppercase">ვებ პროგრამირება</h1>
           <p className="py-5 w-[768px] text-[20px] leading-normal font-normal">
-            ვებ პროგრამირების 10 თვიანი კურსი მსმენელს საფუძვლიანად შეასწავლის
-            JavaScript, React JS და Node Js ტექნოლოგიას, რაც აუცილებელია
-            ვებსაიტების დამოუკიდებლად აწყობისა და ფუნქციურად გამართვისათვის,
-            ასევე http web სერვერების პროგრამირებისათვის. ამასთან, კურსის
-            ფარგლებში მსმენელი დაეუფლებს ისეთ მიმართულებებს, როგორებიცაა:
+          ვებ პროგრამირების 10 თვიანი კურსი მსმენელს საფუძვლიანად შეასწავლის Javasctipt, React JS და Node Js ტექნოლოგიას, რაც აუცილებელია ვებსაიტების დამოუკიდებლად აწყობისა და ფუნქციურად გამართვისათვის, ასევე http web სერვერების პროგრამირებისათვის. ამასთან, კურსის ფარგლებში მსმენელი დაეუფლება ისეთ მიმართულებებს, როგორებიცაა:
           </p>
           <nav>
             <ul className="list-disc pl-4 text-[20px] leading-normal font-normal">
@@ -52,38 +39,38 @@ export default function AnimationDiv() {
           <Image src={mainmain} width={568} height={300} alt="img" />
         </div>
       </section>
-      
-      <section className={`flex justify-between pt-[72px] relative z-20 ${isHovered ? 'text-black' : 'text-white'}`}>
+
+      <section className="flex justify-between pt-[72px] relative z-20">
         <div className={`flex gap-5 transition-transform duration-300 ${isHovered ? 'transform translate-y-[-20px]' : ''}`}>
           <motion.div
-            whileHover={{  translateY: 20 }}
-            transition={{ duration: 0.3 }} 
+            whileHover={{ translateY: 20 }}
+            transition={{ duration: 0.3 }}
           >
             <Image src={calendar} width={30} height={30} alt="calendar" />
             <h2 className="pt-[10px] text-[16px] leading-normal font">კურსის ხანგრძლივობა</h2>
             <h3 className="text-[12px] leading-normal font-medium">10 თვე</h3>
           </motion.div>
           <motion.div
-            whileHover={{  translateY: 20 }}
-            transition={{ duration: 0.3 }} 
+            whileHover={{ translateY: 20 }}
+            transition={{ duration: 0.3 }}
           >
             <Image src={clock} width={30} height={30} alt="clock" />
             <h2 className="pt-[10px] text-[16px] leading-normal font">შეხვედრების რაოდენობა</h2>
             <h3 className="text-[12px] leading-normal font-medium">100 შეხვედრა</h3>
           </motion.div>
           <motion.div
-            whileHover={{  translateY: 20 }}
-            transition={{ duration: 0.3 }} 
+            whileHover={{ translateY: 20 }}
+            transition={{ duration: 0.3 }}
           >
             <Image src={aim} width={30} height={30} alt="aim" />
             <h2 className="pt-[10px] text-[16px] leading-normal font">კურსის მიზანი</h2>
             <h3 className="w-[300px] text-[12px] leading-normal font-medium">
-              კურსის მიზანია მსმენელი გახდეს junior full stack დეველოპერი{" "}
+              კურსის მიზანია მსმენელი გახდეს junior full stack დეველოპერი
             </h3>
           </motion.div>
           <motion.div
-            whileHover={{  translateY: 20 }}
-            transition={{ duration: 0.3 }} 
+            whileHover={{ translateY: 20 }}
+            transition={{ duration: 0.3 }}
           >
             <Image src={pricetag} width={30} height={30} alt="pricetag" />
             <h2 className="pt-[10px] text-[16px] leading-normal font">საფასური</h2>
@@ -95,6 +82,15 @@ export default function AnimationDiv() {
           <h2 className="bg-[#91C6EA] py-2 px-4 rounded-[10px] text-[24px] leading-normal font-medium">მოითხოვე კონსულტაცია</h2>
         </div>
       </section>
+      <div 
+        className={`absolute bottom-0 left-0 w-full h-full transition-all duration-1000 ease-in-out z-10 ${isHovered ? 'clip-path-full' : 'clip-path-initial'}`}
+        style={{
+          backgroundColor: 'white',
+          clipPath: isHovered
+            ? 'polygon(0% 100%, 0% 0%, 100% 0%, 100% 100%)'
+            : 'polygon(0% 99%, 67% 78%, 100% 52%, 99% 99%)',
+        }}
+      />
     </main>
   );
 }
